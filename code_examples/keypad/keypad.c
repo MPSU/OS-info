7010b16fec9f627a5fa24c8ea77cf03d0a3dc0ad
SergeyBalabaev <sergei.balabaev@mail.ru>
2022-11-12 17:00:26 +0300

    add  another library

--- code_examples/keypad/keypad.c
+++ code_examples/keypad/keypad.c
@@ -23,34 +23,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pigpio.h>
 #define ROWS 4
 #define COLS 3
 
 char pressedKey = '\0';
-
-#define VAR8
-
 #ifdef VAR6
-int rowPins[ROWS] = { 18, 17, 16, 25 }; // R0, R1, R2, R3				
-int colPins[COLS] = { 20, 21, 19 };  // C0, C1, C2				
+int rowPins[ROWS] = { 18, 17, 16, 25 }; // R0, R1, R2, R3
+int colPins[COLS] = { 20, 21, 19 }; // C0, C1, C2
 #endif
 
 #ifdef VAR8
-int rowPins[ROWS] = { 4, 25, 11, 8 }; // R0, R1, R2, R3	
+int rowPins[ROWS] = { 4, 25, 11, 8 }; // R0, R1, R2, R3
 int colPins[COLS] = { 7, 6, 5 }; // C0, C1, C2
 #endif
 
 #ifdef VAR10
-int rowPins[ROWS] = { 18, 17, 16, 25 }; // R0, R1, R2, R3				
-int colPins[COLS] = { 20, 21, 19 };  // C0, C1, C2			
+int rowPins[ROWS] = { 18, 17, 16, 25 }; // R0, R1, R2, R3
+int colPins[COLS] = { 20, 21, 19 }; // C0, C1, C2
+#endif
+
+#ifdef BCM
+#include <bcm2835.h>
+#else
+#include <pigpio.h>
 #endif
 
 char keys[ROWS][COLS] = { { '1', '2', '3' },
-			  			  { '4', '5', '6' },
-			  			  { '7', '8', '9' },
-			  			  { '*', '0', '#' } };
+			  { '4', '5', '6' },
+			  { '7', '8', '9' },
+			  { '*', '0', '#' } };
 
+#ifdef BCM
+void init_keypad()
+{
+	for (int c = 0; c < COLS; c++) {
+		bcm2835_gpio_fsel(colPins[c], BCM2835_GPIO_FSEL_INPT);
+		bcm2835_gpio_write(colPins[c], LOW);
+	}
+
+	for (int r = 0; r < ROWS; r++) {
+		bcm2835_gpio_fsel(rowPins[r], BCM2835_GPIO_FSEL_OUTP);
+		bcm2835_gpio_write(rowPins[r], HIGH);
+	}
+}
+
+int findHighCol()
+{
+	for (int c = 0; c < COLS; c++) {
+		if (bcm2835_gpio_lev(colPins[c]) == 1)
+			return c;
+	}
+	return -1;
+}
+
+char get_key()
+{
+	int colIndex;
+	for (int r = 0; r < ROWS; r++) {
+		bcm2835_gpio_write(rowPins[r], HIGH);
+		colIndex = findHighCol();
+		if (colIndex > -1) {
+			if (!pressedKey)
+				pressedKey = keys[r][colIndex];
+			return pressedKey;
+		}
+		bcm2835_gpio_write(rowPins[r], LOW);
+	}
+	pressedKey = '\0';
+	return pressedKey;
+}
+
+#else
 void init_keypad()
 {
 	for (int c = 0; c < COLS; c++) {
@@ -92,6 +135,8 @@ char get_key()
 	return pressedKey;
 }
 
+#endif
+
 void help()
 {
 	printf("    Use this application for keypad\n");
@@ -115,22 +160,40 @@ int main(int argc, char *argv[])
 			return 0;
 		}
 	}
+#ifdef BCM
+	bcm2835_init();
+#else
 	gpioInitialise();
+#endif
+
 	init_keypad();
-	system("clear");
+	if (!quiet)
+		system("clear");
 	while (1) {
 		char x = get_key();
 		if (x) {
-			if (!quiet)
+			if (!quiet) {
+				system("clear");
 				printf("pressed: %c\n", x);
-			else
+			} else
 				printf("%c\n", x);
-		} else if (!quiet)
+		} else if (!quiet) {
+			system("clear");
 			printf("no key pressed\n");
+		}
+#ifdef BCM
+		bcm2835_delay(500);
+#else
 		time_sleep(0.5);
+#endif
 		fflush(stdout);
-		system("clear");
 	}
+
+#ifdef BCM
+	bcm2835_close();
+#else
 	gpioTerminate();
+#endif
+
 	return 0;
 }
